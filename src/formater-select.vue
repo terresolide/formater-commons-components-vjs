958d12c01ed1ba78079ce16394d4d828f9c973c4
POINTAL Elisabeth <pointal@ipgp.fr>
2017-11-10 15:30:59 +0100

    add prop width and color to formater-select

--- src/formater-select.vue
+++ src/formater-select.vue
@@ -6,7 +6,6 @@
 </div>
 </template>
 <script>
-
 export default {
     props:{
         options:{
@@ -16,10 +15,14 @@ export default {
             type:String,
             default:"select"
         },
-        parent:{
-            type:String,
-            default:''
-        }
+       width:{
+           type:String,
+           default:"auto"
+       },
+      color:{
+           type: String,
+           default:null
+       }
     },
   
     data(){
@@ -34,122 +37,142 @@ export default {
     },
     watch:{
         value:function(ev){
-            console.log( "emit   " + this.value);
             this.$emit( 'input', this.value);
         }
     },
     
     created: function(){
         var options = JSON.parse( this.options.replace(/'/g, '"'))
-        this.value = options[0];
-       
+        this.value = options[0];   
         this.indexes = options;
-        this.$emit( 'input', this.value);
-        this.resetEventListener = this.handleReset.bind(this) 
-        document.addEventListener('formaterResetEvent', this.resetEventListener);
-        this.searchEventListener = this.handleSearch.bind(this) 
-        document.addEventListener('formaterSearchEvent', this.searchEventListener);
-        this.aerisThemeListener = this.handleTheme.bind(this) 
-        document.addEventListener('aerisTheme', this.aerisThemeListener);
-        
+        this.$emit( 'input', this.value); 
+        this.initListeners();     
+    },
+    mounted: function(){
+        this.initCss();
     },
     destroyed: function() {
-    	document.removeEventListener('formaterResetEvent', this.resetEventListener);
-    	this.resetEventListener = null;
-    	document.removeEventListener('formaterSearchEvent', this.searchEventListener);
-    	this.searchEventListener = null;
-    	 document.removeEventListener('aerisTheme', this.aerisThemeListener);
-         this.aerisThemeListener = null;
+    	this.removeListeners();
       },
       
-      methods:{
-          handleReset: function(evt){
-              this.value = indexes[0];
-      	  },
-          handleSearch: function(evt){
-              evt.detail[this.name] = this.value;
-          },
-          handleTheme: function(theme) {
-		  		this.theme = theme.detail
-				this.ensureTheme()
+    methods:{
+        handleReset: function(evt){
+            this.value = indexes[0];
+    	},
+        handleSearch: function(evt){
+            evt.detail[this.name] = this.value;
+        },
+        handleTheme: function(theme) {
+	  		this.theme = theme.detail
+			this.ensureTheme()
 		},
 		  	
-		 ensureTheme: function() {
-		  	if ((this.$el) && (this.$el.querySelector)) {
-		  	    console.log('ensureTheme select');
-		  		var color3 =  this.$shadeColor( this.theme.primary, 0.8);
-		  		this.$el.querySelector(".formater-select select").style.backgroundColor = color3;
-		  	}
-		 },
-      }
+		ensureTheme: function() {
+		 	if ( !this.color &&  (this.$el) && (this.$el.querySelector)) {
+		 		var color3 =  this.$shadeColor( this.theme.primary, 0.8);
+		 		this.$el.querySelector("select").style.backgroundColor = color3;
+		 	}
+		},
+		initCss: function(){
+	        if ((this.$el) && (this.$el.querySelector)) {
+	            this.$el.querySelector("select").style.width = this.width;
+	            this.$el.style.width = this.width;
+	            if(this.color){
+	                console.log(this.color);
+	                this.$el.querySelector("select").style.backgroundColor = this.color;
+	            }
+	        }
+    	},
+	    initListeners: function(){
+	        this.resetEventListener = this.handleReset.bind(this) 
+	        document.addEventListener('aerisResetEvent', this.resetEventListener);
+	        this.searchEventListener = this.handleSearch.bind(this) 
+	        document.addEventListener('aerisSearchEvent', this.searchEventListener);
+	        this.aerisThemeListener = this.handleTheme.bind(this) 
+	        document.addEventListener('aerisTheme', this.aerisThemeListener);
+	    },
+	    removeListeners: function(){
+			document.removeEventListener('aerisResetEvent', this.resetEventListener);
+			this.resetEventListener = null;
+			document.removeEventListener('aerisSearchEvent', this.searchEventListener);
+			this.searchEventListener = null;
+			document.removeEventListener('aerisTheme', this.aerisThemeListener);
+	        this.aerisThemeListener = null;
+	    }
+	}
 
 }
 
 </script>
 <style>
-.formater-select select{
-    font-size: inherit; /* inherit size from .custom-dropdown */
-    padding: .5em; /* add some space*/
-    margin: 0; /* remove default margins */
+.formater-select {
+	position: relative;
+	display: inline-block;
+	vertical-align: middle;
 }
 
-@supports (pointer-events: none) and
-      ((-webkit-appearance: none) or
-      (-moz-appearance: none) or
-      (appearance: none)) {
+.formater-select select {
+	font-size: inherit; /* inherit size from .custom-dropdown */
+	padding: .5em; /* add some space*/
+	margin: 0; /* remove default margins */
+}
 
-    .formater-select {
-        position: relative;
-        display: inline-block;
-        vertical-align: middle;
-    }
-
-    .formater-select select{
-        padding-right: 2.5em; /* accommodate with the pseudo elements for the dropdown arrow */
-        border: 0;
-        border-radius: 1px;
-        -webkit-appearance: none;
-        -moz-appearance: none;
-        appearance: none; 
-        /* Default color from aeris */
-         background-color: rgba(172,220,238,0.3); 
-        width:260px;  
-    }
-
-    .formater-select::before,
-    .isgi-select::after {
-        content: "";
-        position: absolute;
-        pointer-events: none;
-    }
-
-    .formater-select::after { /*  Custom dropdown arrow */
-        content: "\25BC";
-        height: 1em;
-        font-size: .625em;
-        line-height: 1;
-        right: 1.2em;
-        top: 50%; margin-top: -.5em;
-    }
-
-    .formater-select::before { /*  Custom dropdown arrow cover */
-        width: 2em;
-        right: 0; top: 0; bottom: 0;
-        border-radius: 0 3px 3px 0;
-    }
-
-    .formater-select select[disabled] {
-        color: rgba(0,0,0,.3);
-    }
-
-    .isgi-select select[disabled]::after {
-        color: rgba(0,0,0,.1);
-    }
-
-
-    /* FF only temp fix */
-    @-moz-document url-prefix() {
-        .isgi-select select{ padding-right: .9em }
-    }
+@supports (pointer-events: none ) and
+      ((-webkit-appearance: none ) or
+      (-moz-appearance: none ) or
+      (appearance: none )) { 
+      
+	.formater-select { position:relative;
+		display: inline-block;
+		vertical-align: middle;
+	}
+	
+	.formater-select select {
+		padding-right: 2.5em;
+		/* accommodate with the pseudo elements for the dropdown arrow */
+		border: 0;
+		border-radius: 1px;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		appearance: none;
+		/* Default color from aeris */
+		background-color: rgba(172, 220, 238, 0.3);
+	}
+	
+	.formater-select::before, .formater-select::after {
+		content: "";
+		position: absolute;
+		pointer-events: none;
+		box-sizing: border-box;
+	}
+	
+	.formater-select::after { /*  Custom dropdown arrow */
+		content: "\25BC";
+		height: 1em;
+		font-size: .625em;
+		line-height: 1;
+		right: 1.2em;
+		top: 50%;
+		margin-top: -.5em;
+	}
+	
+	.formater-select::before { /*  Custom dropdown arrow cover */
+		width: 2em;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		border-radius: 0 3px 3px 0;
+	}
+	
+	.formater-select select[disabled] {
+		color: rgba(0, 0, 0, .3);
+	}
+	
+	.isgi-select select[disabled]::after {
+		color: rgba(0, 0, 0, .1);
+	}
+	
+	/* FF only temp fix */
+	@-moz-document url-prefix () { .isgi-select select{ padding-right:.9em}}
 }
 </style>
