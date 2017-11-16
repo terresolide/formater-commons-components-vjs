847b3f904e0c68104216f0e7b07bc906af83dc2f
POINTAL Elisabeth <pointal@ipgp.fr>
2017-11-16 14:18:42 +0100

    test version too

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: buildName+'1.js'
+    filename: buildName+'.js'
   },
   module: {
     rules: [
