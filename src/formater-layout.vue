f4160ac96debabdf6d73ca0d96f64fa71e350bd4
POINTAL Elisabeth <pointal@ipgp.fr>
2017-11-28 16:28:39 +0100

    modify layout

--- src/formater-layout.vue
+++ src/formater-layout.vue
@@ -4,79 +4,95 @@
 	<slot></slot>
 	</span>
 </template>
+<script>
+export default {
+data(){
+    return {
+ 		 aerisThemeListener:null,
+         theme:null
+    }
+},
+methods: {
 
+	handleTheme: function(theme) {
+	    console.log("formater layout handle theme");
+	  		this.theme = theme.detail
+			this.ensureTheme()
+	},
+	  	
+	 ensureTheme: function() {
+	  	if ((this.$el) && (this.$el.querySelector)) {
+	  		this.$el.querySelector(".formater-layout > header").style.background= this.theme.primary;
+  		}
+	 }
+ },
+created: function(){
+   
+    this.aerisThemeListener = this.handleTheme.bind(this) 
+    document.addEventListener('aerisTheme', this.aerisThemeListener);
+
+},
+mounted: function(){
+    
+    var event = new CustomEvent('aerisThemeRequest', {});
+   	document.dispatchEvent(event);
+ },
+ destroyed: function(){
+     document.removeEventListener('aerisTheme', this.aerisThemeListener);
+     this.aerisThemeListener = null;
+ }
+ }
+</script>
 
 <style>
-/*.formater-header{
-	grid-aera: header;
-	}
-	.formater-sidebar{
-		grid-aera: sidebar;
-	}
-	.formater-content{
-		grid-aera: content;
-	}
-	.formater-footer{
-		grid-aera: footer;
-	}
+
 .formater-layout{
-	display: grid;
-	grid-template-columns: 100%;
+	display:block;
+}
+.formater-layout > header{
+	color:#fff;
+	background: blue;
+	padding:  10px;
+	margin: 0;
+}
+.formater-layout > header h1,
+.formater-layout > header h2,
+.formater-layout > header h3{
+	padding:0;
+	-webkit-margin-before: 0em;
+    -webkit-margin-after: 0em;
+}
+.formater-layout .formater-wrapper{
+	 display: block;
+	-webkit-box-flex: 1;
+	-ms-flex: 1 1 auto;
+	flex: 1 1 auto;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-orient: horizontal;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: row;
+	flex-direction: row;
+}
+
+.formater-layout  > header,
+.formater-layout > footer{
+    width:100%;
+
+}
 	
-}*/
-.formater-layout{
-  width:100%;
-  padding:3px;
+.formater-layout aside{
+    
+	display:block;
+	
+}
+.formater-layout main{
+	 display: block;
+	-webkit-box-flex: 1;
+	-ms-flex: 1;
+	flex: 1;
 }
 
-.formater-layout .formater-header,
-.formater-layout .formater-sidebar,
-.formater-layout .formater-content,
-.formater-layout .formater-footer{
-	width:100%;
-}
 
-@media screen and (min-width:720px){
-	/*.formater-layout{
-		display: grid;
-		grid-template-columns: 300px 0.9fr;
-		grid-template-rows: auto auto auto;
-		grid-template-areas:
-		"header header
-		sidebar content
-		footer footer";
-	}*/
-	.formater-layout aside,
-	.formater-layout main{
-		display:inline-block;
-	}
-	.formater-layout .formater-header,
-	.formater-layout .formater-footer{
-		width:100%;
-		clear:both;
-	}
-	.formater-layout .formater-wrapper{
-		text-align:center;
-	}
-
-	.formater-layout .formater-sidebar{
-		display:inline-block;
-		text-align:left;
-		vertical-align:top;
-		width:300px;
-	}
-	.formater-layout .formater-content{
-		display:inline-block;
-		text-align:left;
-		vertical-align:top;
-		width:320px;
-	}
-
-}
-
-@media screen and (min-width:1024px){
-	.formater-layout .formater-content{
-		width:720px;
-	}
-}
 </style>
