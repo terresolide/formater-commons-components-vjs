c870126ead3faab5623f410a3f54d9b026cbe442
epointal <elisabeth.pointal@free.fr>
2018-10-01 14:40:05 +0200

    test to easily import component

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Export components
+ */
+
+export FormaterSearchBox from './src/formater-search-box.vue'
+export FormaterSelect from './src/formater-select.vue'
+export FormaterTemporalSearch from './src/formater-temporal-search.vue'
+export FormaterTools from './src/formater-tools.js'
