312862ae2eda3fa0d6f03bb3a1fb1cd489788773
epointal <elisabeth.pointal@free.fr>
2018-10-18 17:09:25 +0200

    add new component

--- src/drag-drop-file.vue
+++ src/drag-drop-file.vue
@@ -2,18 +2,53 @@
  * Area where drag and drop file
  * @return file content?
  */
+<i18n>
+{ 
+  "en": {
+    "drop_files_here": "Drop your files here",
+    "select": "Select",
+    "remove": "Remove all"
+  },
+  "fr": {
+    "drop_files_here": "Déposez vos fichiers ici",
+    "select": "Sélectionner",
+    "remove": "Effacer"
+  }
+}
+</i18n>
 <template>
- <span class="dragdrop-file">
- <formater-alert-message :msg="JSON.stringify(alert)" :playing="false"></formater-alert-message>
- <div @drop="dropHandler" @dragover="dragOverHandler">
- </div>
+<span>
+	<formater-alert-message :msg="JSON.stringify(alert)" :playing="false"></formater-alert-message>
+	<div class="upload-buttons-wrapper">
+	    <div id="pick-files" role="button" style="z-index: 1;" @click="$refs.file.click()">
+	      <i class="fa fa-folder-open"></i>
+	      <span class="ui-button-text">{{$t('select')}}</span>
+	    </div>
+	    <div id="reset-all" role="button" @click="removeAll()">
+	       <span class="fa fa-close"></span>
+	       <span class="ui-button-text">{{$t('remove')}}</span>
+	    </div>
+ 	    <div class="select-file">
+ 	      <input id="input-file" ref="file" type="file" class="hidden-input" @change="readUrl"/>
+ 	    </div>
+ 	</div>
+	<span class="dragdrop-file">
+	  <div @drop="dropHandler" @dragover="dragOverHandler">
+	  <div class="files-container">
+	     <formater-file v-for="(file,index) in files" :key="index" :filename="file.name"></formater-file>
+	  </div>
+      <span v-show="this.files.length === 0">{{$t('drop_files_here')}}</span>
+	</div>
+	 </span>
  </span>
 </template>
 <script>
 import FormaterAlertMessage from './formater-alert-message.vue'
+import FormaterFile from './formater-file.vue'
 export default {
   components: {
-    FormaterAlertMessage
+    FormaterAlertMessage,
+    FormaterFile
   },
   name: 'DragdropFile',
   props: {
@@ -29,10 +64,16 @@ export default {
 
   data () {
     return {
-      alert: []
+      alert: [],
+      extension: [],
+      files: [{name:'machin.png'}]
     }
   },
-  mounted () {
+  created () {
+    this.$i18n.locale = this.lang
+  },
+  mounted () { 
+    this.extension = this.ext.split(',')
   },
   destroyed () {
   },
@@ -51,33 +92,124 @@ export default {
             var file = event.dataTransfer.items[i].getAsFile();
             console.log('... file[' + i + '].name = ' + file.name);
             console.log(file)
+            this.receiveFile(file)
           }
         }
       } else {
         // Use DataTransfer interface to access the file(s)
         for (var i = 0; i < event.dataTransfer.files.length; i++) {
           console.log('... file[' + i + '].name = ' + event.dataTransfer.files[i].name);
+          this.receiveFile(file)
         }
       } 
   
       // Pass event to removeDragData for cleanup
       // removeDragData(event)
     },
+    readUrl (event) {
+      event.preventDefault()
+      var file = event.target.files[0]
+      this.receiveFile(file)
+    },
+    receiveFile (file) {
+      console.log(ftTools.getExtension(file.name))
+      console.log(this.extension.indexOf(ftTools.getExtension(file.name)) )
+      if (this.extension.indexOf(ftTools.getExtension(file.name)) >= 0) {
+        console.log('fichier valide')
+      }
+
+    },
     dragOverHandler (event) {
       event.preventDefault()
      // console.log(event)
+    },
+    removeAll () {
+      console.log('remove all files')
     }
   }
 }
 </script>
 <style>
+  
   .dragdrop-file {
+      position:relative;
+      display:inline-block;
       width:100%;
-      height:200px;
-      display:block;
-      background: grey;
+      height:150px;
+      background: #eaf4f4;  
   }
   .dragdrop-file > div {
      height: 100%;
+     display:block;
+     line-height:150px;
+     text-align:center;
+  }
+  .dragdrop-file > div > span {
+    vertical-align:middle;
+    color:grey;
+    font-size:20px;
+    width:100%;
+  }
+  .dragdrop-file .files-container {
+    position:absolute;
+    top:0;
+    left:0;
+    height:150px;
+  }
+  div.upload-buttons-wrapper{
+    width: 310px;
+    margin: 0 auto 10px auto;
+    height: 33px;
+    position: relative;
+  }
+  div[id="pick-files"],
+  div[id="reset-all"]{
+    font-size: 13px;
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+    position: absolute;
+    top: 0;
+    width: 145px;
+    background-color: #47A4A5;
+    border-color: #47A4A5;
+    color: #FFFFFF;
+    cursor: pointer;
+    display: inline-block;
+    padding: 2px 7px 3px;
+    margin-bottom: 0;
+    line-height: 1.428571429;
+    text-align: center;
+	white-space: nowrap;
+	vertical-align: middle;
+	cursor: pointer;
+	border: 1px solid #ccc;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	-o-user-select: none;
+	user-select: none;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+	border-radius: 4px;
+  }
+  div[id="reset-all"]{
+    left:180px;
+  }
+  div.select-file {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 145px;
+    height: 33px;
+    overflow: hidden;
+    z-index: 0;
+  }
+  div.select-file input.hidden-input{
+    font-size: 999px;
+    opacity: 0;
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100%;
   }
 </style>
---
+++ src/formater-file.vue
@@ -0,0 +1,82 @@
+/**
+ * Display icon file with its name 
+ * @event remove with index when click on button remove
+ */
+<i18n>
+{ 
+  "en": {
+    "drop_files_here": "Drop your files here",
+    "select": "Select",
+    "remove": "Remove all"
+  },
+  "fr": {
+    "drop_files_here": "Déposez vos fichiers ici",
+    "select": "Sélectionner",
+    "remove": "Effacer"
+  }
+}
+</i18n>
+<template>
+<span class="formater-file">
+	   <div class="file-container">
+	      <i class="fa fa-file"></i>
+	      <div>{{filename}}</div>
+	   </div>
+ </span>
+</template>
+<script>
+
+export default {
+  name: 'FormaterFile',
+  props: {
+    lang: {
+      type: String,
+      default: 'fr'
+    },
+    filename: {
+      type: String,
+      default: 'unknown'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+    this.$i18n.locale = this.lang
+  },
+  mounted () { 
+    this.extension = this.ext.split(',')
+  },
+  destroyed () {
+  },
+  methods: {
+    remove () {
+      console.log('remove')
+    }
+  }
+}
+</script>
+<style scoped>
+.formater-file{
+  display:inline;
+  vertical-align:middle;
+}
+ .file-container{
+   line-height: 1;
+   position: relative;
+   border: 1px dotted grey;
+   padding: 5px;
+   display: block;
+   background: lightgrey;
+   border-radius: 4px;
+ }
+ .file-container i{
+   font-size:60px;
+   color:grey;
+ }
+ .file-container div {
+    font-size: 12px;
+    margin: 10px 0 0 0;
+ }
+</style>
