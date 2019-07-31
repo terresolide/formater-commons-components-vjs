36ca09a3dd1e3c129b6c44d9b3d5760ec8e97812
epointal <elisabeth.pointal@free.fr>
2019-07-31 16:16:56 +0200

    change placeholder date in temporal search

--- src/formater-temporal-search.vue
+++ src/formater-temporal-search.vue
@@ -1,33 +1,33 @@
 <i18n>
 {
   "en": {
-	  "from": "from",
-	  "to": "to",
-	  "inconsistent_dates":	"inconsistent dates",
-	  "equal_dates": "The end date should be greater than the start date"
+      "from": "from",
+      "to": "to",
+      "inconsistent_dates":    "inconsistent dates",
+      "equal_dates": "The end date should be greater than the start date"
   },
   "fr": {
-	  "from": "de",
-	  "to": "à",
-	  "inconsistent_dates":	"dates incohérentes",
-	   "equal_dates": "la date finale doit être supérieure à la date initiale"
+      "from": "de",
+      "to": "à",
+      "inconsistent_dates":    "dates incohérentes",
+       "equal_dates": "la date finale doit être supérieure à la date initiale"
   }
 }
 </i18n>
 
 <template>
 <span class="formater-temporal-search" :class="{disable: disable}">
-	<div class="formater-input-group" >
-	   <span class="right">{{$t('from')}}</span>
-	  <input id="from" v-model="from" @click="errorMessage = null" @input="input"  :placeholder="date2str(daymin)" :pattern="pattern"/>
-	</div>
-	<aeris-datepicker for="input#from" :format="format" :lang="lang"  :daymin="daymin" :daymax="daymax"></aeris-datepicker>
-	<div class="formater-input-group">
-		<span class="right">{{$t('to')}}</span>
-		<input id="to" v-model="to" @click="errorMessage = null" @input="input" :placeholder="now()" :pattern="pattern">
-	</div>
-	<aeris-datepicker for="input#to" :format="format" :lang="lang" :daymin="daymin" :daymax="daymax" ></aeris-datepicker> 
-	<span class="error-message" v-if="errorMessage">{{errorMessage}}</span>
+    <div class="formater-input-group" >
+       <span class="right">{{$t('from')}}</span>
+      <input id="from" v-model="from" @click="errorMessage = null" @input="input"  :placeholder="date2str(daymin)" :pattern="pattern"/>
+    </div>
+    <aeris-datepicker for="input#from" :format="format" :lang="lang"  :daymin="daymin" :daymax="daymax"></aeris-datepicker>
+    <div class="formater-input-group">
+        <span class="right">{{$t('to')}}</span>
+        <input id="to" v-model="to" @click="errorMessage = null" @input="input" :placeholder="date2str(daymax)" :pattern="pattern">
+    </div>
+    <aeris-datepicker for="input#to" :format="format" :lang="lang" :daymin="daymin" :daymax="daymax" ></aeris-datepicker> 
+    <span class="error-message" v-if="errorMessage">{{errorMessage}}</span>
 </span>
 </template>
 
@@ -44,7 +44,7 @@ export default {
     AerisDatepicker
   },
   props: {
-  	lang:  {
+      lang:  {
       type: String,
       default: 'fr'
     },
@@ -53,16 +53,16 @@ export default {
         default:'DD/MM/YYYY'
     },
     pattern:{
-    	type: String,
-    	default:'(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/[0-9]{4}'
+        type: String,
+        default:'(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[012])\/[0-9]{4}'
     },
     daymin:{
-    	type: String,
-    	default:'1970-01-01'
+        type: String,
+        default:'1970-01-01'
     },
     daymax:{
-    	type:String,
-    	default:'now'
+        type:String,
+        default: 'now'
     },
     disable: {
         type: Boolean,
@@ -70,22 +70,22 @@ export default {
     }
   }, 
   destroyed: function() {
-		document.removeEventListener('aerisResetEvent', this.resetEventListener);
-		this.resetEventListener = null;
-		document.removeEventListener('aerisSearchEvent', this.searchEventListener);
-		this.searchEventListener = null;
-		document.removeEventListener('aerisTheme', this.aerisThemeListener);
+        document.removeEventListener('aerisResetEvent', this.resetEventListener);
+        this.resetEventListener = null;
+        document.removeEventListener('aerisSearchEvent', this.searchEventListener);
+        this.searchEventListener = null;
+        document.removeEventListener('aerisTheme', this.aerisThemeListener);
         this.aerisThemeListener = null;
   },
   
   created: function () {
-		this.$i18n.locale = this.lang;
-		this.resetEventListener = this.handleReset.bind(this) 
-		document.addEventListener('aerisResetEvent', this.resetEventListener);
-		this.searchEventListener = this.handleSearch.bind(this) 
-		document.addEventListener('aerisSearchEvent', this.searchEventListener);
-		this.aerisThemeListener = this.handleTheme.bind(this) 
-	    document.addEventListener('aerisTheme', this.aerisThemeListener);
+        this.$i18n.locale = this.lang;
+        this.resetEventListener = this.handleReset.bind(this) 
+        document.addEventListener('aerisResetEvent', this.resetEventListener);
+        this.searchEventListener = this.handleSearch.bind(this) 
+        document.addEventListener('aerisSearchEvent', this.searchEventListener);
+        this.aerisThemeListener = this.handleTheme.bind(this) 
+        document.addEventListener('aerisTheme', this.aerisThemeListener);
   },
 
   mounted: function(){
@@ -97,14 +97,14 @@ export default {
    data () {
     return {
         searchEventListener: null,
-   		resetEventListener: null,
-   		aerisThemeListener:null,
-   		theme:null,
-    	from:null,
-    	to:null,
-    	old:{ from:null, to: null},
-    	errorMessage: null,
-    	hasChanged:false
+           resetEventListener: null,
+           aerisThemeListener:null,
+           theme:null,
+        from:null,
+        to:null,
+        old:{ from:null, to: null},
+        errorMessage: null,
+        hasChanged:false
     }
   },
  
@@ -113,105 +113,109 @@ export default {
 
   
   methods: {
-	  close:function(){
-		  console.log("close");
-	  },
+      close:function(){
+          console.log("close");
+      },
       input: function(e){
-    	  //Impossible de déclencher un événement avec this.$emit sur le composant??
-    	  // Apparemment c'est une question de profondeur.
-    	  var event = new CustomEvent(
-    			  'temporalChangeEvent', 
-    			  {detail:
-    			  	{
-    				  name:e.target.id , 
-    				  value:moment(e.target.value, this.format).format("YYYY-MM-DD")
-    				 }
-    			  });
-    	  document.dispatchEvent(event);
+          //Impossible de déclencher un événement avec this.$emit sur le composant??
+          // Apparemment c'est une question de profondeur.
+          var event = new CustomEvent(
+                  'temporalChangeEvent', 
+                  {detail:
+                      {
+                      name:e.target.id , 
+                      value:moment(e.target.value, this.format).format("YYYY-MM-DD")
+                     }
+                  });
+          document.dispatchEvent(event);
       },
       date2str(  date){
-    	  return moment(date).format(this.format);
+          if (date === 'now') {
+            return moment().format(this.format)
+          } else {
+            return moment(date).format(this.format)
+          }
       },
       str2date( str, format){
-    	  if( str == "now"){
-    		  return moment();
-    	  }else{
-    		  return moment( str, format);
-    	  }
+          if( str == "now"){
+              return moment();
+          }else{
+              return moment( str, format);
+          }
       },
       now(){
-    	  return this.date2str( moment());
+          return this.date2str( moment());
       },
-	handleReset: function() {
-		 this.from = ""
-		 this.$el.querySelector('#from').value = ""
-		 this.to = ""
-		 this.$el.querySelector('#to').value = ""
-	},
-	
-	handleSearch: function(e) {
-		if( this.last ){
-			this.from = "";
-			this.to = "";
-		    return;
-		}
-		//v-model not working??
-		 
-		this.from =  this.$el.querySelector('#from').value
-		this.to =  this.$el.querySelector('#to').value
-		var fromDefault = false;
-		var toDefault = false
+    handleReset: function() {
+         this.from = ""
+         this.$el.querySelector('#from').value = ""
+         this.to = ""
+         this.$el.querySelector('#to').value = ""
+    },
+    
+    handleSearch: function(e) {
+        if( this.last ){
+            this.from = "";
+            this.to = "";
+            return;
+        }
+        //v-model not working??
+         
+        this.from =  this.$el.querySelector('#from').value
+        this.to =  this.$el.querySelector('#to').value
+        var fromDefault = false;
+        var toDefault = false
 
-		if( this.from == "" ){
-			var from = this.str2date( this.daymin, "YYYY-MM-DD");
-			var fromDefault = true
-		}else{
-			var from = moment(this.from, this.format);
-		}
-		if( this.to == ""){
-			var to = this.str2date( this.daymax, "YYYY-MM-DD");
-			var toDefault = true
-		}else{
-			var to = moment(this.to, this.format);
-		}
-		
-		
-	
-		if(!from.isValid() || !to.isValid() || from > to ){
-		    this.errorMessage = this.$i18n.t('inconsistent_dates');
-		   	e.detail.error = true;
-	    }else{
-		
-			var str_from = from.format('YYYY-MM-DD');
-			var str_to = to.format('YYYY-MM-DD');
-			
-			if(str_from ){
-				e.detail.start = str_from;
-			}
-			if(str_to){
-			
-				e.detail.end = str_to;
-			}
-			e.detail.startDefault = fromDefault
-			e.detail.endDefault = toDefault
-	    }
-	  },
-	 
+        if( this.from == "" ){
+            var from = this.str2date( this.daymin, "YYYY-MM-DD");
+            var fromDefault = true
+        }else{
+            var from = moment(this.from, this.format);
+        }
+        if( this.to == ""){
+            var to = this.str2date( this.daymax, "YYYY-MM-DD");
+            var toDefault = true
+        }else{
+            var to = moment(this.to, this.format);
+        }
+        
+        
+    
+        if(!from.isValid() || !to.isValid() || from > to ){
+            this.errorMessage = this.$i18n.t('inconsistent_dates');
+               e.detail.error = true;
+        }else{
+        
+            var str_from = from.format('YYYY-MM-DD');
+            var str_to = to.format('YYYY-MM-DD');
+            
+            if(str_from ){
+                e.detail.start = str_from;
+            }
+            if(str_to){
+            
+                e.detail.end = str_to;
+            }
+            e.detail.startDefault = fromDefault
+            e.detail.endDefault = toDefault
+        }
+      },
+     
       handleTheme: function(theme) {
-	  		this.theme = theme.detail;
-			this.ensureTheme();
-	  },
-	  	
-	 ensureTheme: function() {
-	  	if ((this.$el) && (this.$el.querySelector)) {
-	  		var color3 =  this.$shadeColor( this.theme.emphasis, 0.8);
-	  		var nodes= this.$el.querySelectorAll(".formater-input-group");
-	  		[].forEach.call(nodes, function(node){
-	  		    node.style.backgroundColor = color3;
-	  		})
-	  		
-	  	}
-	 }
+              this.theme = theme.detail;
+            this.ensureTheme();
+      },
+          
+     ensureTheme: function() {
+          if ((this.$el) && (this.$el.querySelector)) {
+              var color3 =  this.$shadeColor( this.theme.emphasis, 0.8);
+              var nodes= this.$el.querySelectorAll(".formater-input-group");
+              [].forEach.call(nodes, function(node){
+                  node.style.backgroundColor = color3;
+              })
+              
+          }
+     }
   }
 }
  
@@ -230,9 +234,9 @@ export default {
 }
 
 .formater-temporal-search .right {
-	min-width: 40px;
-	border-right: 1px solid #fff;
-	box-sizing: border-box;
+    min-width: 40px;
+    border-right: 1px solid #fff;
+    box-sizing: border-box;
     display: block;
     height: 100%;
     text-align: center;
@@ -243,7 +247,7 @@ export default {
     color: red
 }
 .formater-temporal-search .error-message::first-letter{
-	text-transform:uppercase;
+    text-transform:uppercase;
 }
 .formater-temporal-search .formater-input-group {
     border: none;
@@ -253,13 +257,13 @@ export default {
 }
 
 .formater-temporal-search .formater-input-group input {
-	border: none;
-	background-color: transparent;
-	padding: 0 5px;
-	outline: none;
-	width:70%;
+    border: none;
+    background-color: transparent;
+    padding: 0 5px;
+    outline: none;
+    width:70%;
 }
-	
+    
 .formater-temporal-search .formater-input-group span:first-letter {
    text-transform: uppercase;
 }
